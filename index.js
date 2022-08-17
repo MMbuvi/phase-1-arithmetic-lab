385891ec65506c38a7992a8eada7fdbe5073951d
Meshack <mbuvi.meshack99@gmail.com>
2022-08-17 20:19:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1= 31;
+let num2=2;
+let multiply = (num1 * num2);
+
+let random = 1;
+Math.ceil(1);
+
+let num3= 8;
+let num4=2;
+let mod = (num3 / num4);
+
+let max = 20
+Math.max(1,2,5,7,9,14,20);
+
+
+
+
+
+
